30165226e7e0f2987675d7cd61ddaa85ff35119b
wen xuan <82707429+wenxuan1011@users.noreply.github.com>
2022-06-16 16:13:32 +0800

    add exercise time in output

--- result_page.py
+++ result_page.py
@@ -31,46 +31,45 @@ class Result_Page():
 
         # create output labels
         if(self.number_list[0] != self.number_list[1]):
-            self.output1_label = tk.Label(self.frame, text = self.data.iat[self.number_list[0], 0],\
+            self.output1_label = tk.Label(self.frame, text = self.data.iat[self.number_list[0], 0]+'  ...  '+str(float(self.duration)/2)+'  hr',\
                 font = ('Candara', 14, 'bold'), bg = 'white')
-            self.bak.create_window(190, 220, anchor=tk.NW, window = self.output1_label)
-            self.output2_label = tk.Label(self.frame, text = self.data.iat[self.number_list[1], 0],\
+            self.bak.create_window(170, 220, anchor=tk.NW, window = self.output1_label)
+            self.output2_label = tk.Label(self.frame, text = self.data.iat[self.number_list[1], 0]+'  ...  '+str(float(self.duration)/2)+'  hr',\
                 font = ('Candara', 14, 'bold'), bg = 'white')
-            self.bak.create_window(190, 265, anchor=tk.NW, window = self.output2_label)
+            self.bak.create_window(170, 265, anchor=tk.NW, window = self.output2_label)
 
             # create output pictures in the window
             img_output2 = Image.open('image/'+str(self.data.iat[self.number_list[1], 2])+'.png').resize((210, 210))
             photo_output2 = ImageTk.PhotoImage(img_output2)
-            self.bak.create_image(525, 220, anchor=tk.NW, image = photo_output2)
+            self.bak.create_image(545, 210, anchor=tk.NW, image = photo_output2)
             img_output1 = Image.open('image/'+str(self.data.iat[self.number_list[0], 2])+'.png').resize((210, 210))
             photo_output1 = ImageTk.PhotoImage(img_output1)
-            self.bak.create_image(430, 60, anchor=tk.NW, image = photo_output1)
+            self.bak.create_image(450, 50, anchor=tk.NW, image = photo_output1)
         
         else:
-            self.output_label = tk.Label(self.frame, text = self.data.iat[self.number_list[0], 0],\
+            self.output_label = tk.Label(self.frame, text = self.data.iat[self.number_list[0], 0]+'  ...  '+str(self.duration)+'  hr',\
                 font = ('Candara', 14, 'bold'), bg = 'white')
-            self.bak.create_window(190, 240, anchor=tk.NW, window = self.output_label)
+            self.bak.create_window(170, 240, anchor=tk.NW, window = self.output_label)
 
             # create output pictures in the window
-            img_output = Image.open('image/'+str(self.data.iat[self.number_list[0], 2])+'.png').resize((250, 250))
+            img_output = Image.open('image/'+str(self.data.iat[self.number_list[0], 2])+'.png').resize((240, 240))
             photo_output = ImageTk.PhotoImage(img_output)
-            self.bak.create_image(470, 120, anchor=tk.NW, image = photo_output)
+            self.bak.create_image(500, 120, anchor=tk.NW, image = photo_output)
         
         percent = round((float(self.fitness)/float(self.calories))*100, 2)
         self.fitness_label = tk.Label(self.frame, text = 'Error:   '+str(round(self.fitness, 2))+'   Kcal   ('+str(percent)+')%',\
             font = ('Candara', 14, 'bold'), bg = 'white')
-        self.bak.create_window(190, 330, anchor=tk.NW, window = self.fitness_label)
+        self.bak.create_window(170, 330, anchor=tk.NW, window = self.fitness_label)
 
         if(round(float(self.fitness)/float(self.calories), 2) > 0.1):
             self.suggest_label = tk.Label(self.frame, text = '( Please adjust your intensity, duration, or calories to fit your goal. )',\
                 font = ('Candara', 11, 'bold'), bg = 'white')
-            self.bak.create_window(155, 420, anchor=tk.NW, window = self.suggest_label)
+            self.bak.create_window(135, 390, anchor=tk.NW, window = self.suggest_label)
 
-
-        # create a button to go to next page
-        self.button = tk.Button(self.frame, width = 8, height = 1, text = 'RESTART', \
+        # create a button to go back to the first page
+        self.button = tk.Button(self.frame, width = 10, height = 1, text = 'RESTART', \
                                       font = ('Arial', 18, 'bold'), border=0, command = self.restart)
-        self.bak.create_window(610, 400, anchor=tk.NW, window=self.button)
+        self.bak.create_window(590, 400, anchor=tk.NW, window=self.button)
 
         self.frame.mainloop()
         pass
